88a8f7123111d5bd649a65f5498ffd5b09b6b02c
Satheesh Kumar <Satheesh@anbsystems.com>
2022-02-22 19:37:02 +0530

    jwt implementation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    console.log(process.env.PORT);
-    console.log(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
-
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
